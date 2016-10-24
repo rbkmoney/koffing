5636d6c21ee9f02570386e7028cef44d657a45c6
Max Minin <m.minin@rbkmoney.com>
2016-10-24 18:41:29 +0300

    изначальная структура для работы с webpack

--- app/components/analytics/finance/search-form/search-form.js
+++
@@ -1,9 +0,0 @@
-const searchForm = angular.module('searchForm', ['datepicker', 'customSelect']);
-
-searchForm.component('searchForm', {
-    templateUrl: 'components/analytics/finance/search-form/search-form.html',
-    bindings: {
-        searchParams: '=',
-        onSearch: '&'
-    }
-});
--- app/shared/resources/shops.js
+++
@@ -1,7 +0,0 @@
-resources.factory('Shops', function ($resource, appConfig) {
-    return $resource(`${appConfig.capiUrl}/processing/shops/:shopID/:action`, {
-        shopID: '@shopID'
-    }, {
-        activate: {method: 'PUT', params: {action: 'activate'}}
-    });
-});
