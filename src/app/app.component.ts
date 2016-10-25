2ea45fee097787eae2f509f7cc57761887c3405a
Max Minin <m.minin@rbkmoney.com>
2016-10-25 19:08:22 +0300

    FE-71: pug templates added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html',
+    selector: 'app',
+    templateUrl: './app.component.pug',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent { }
