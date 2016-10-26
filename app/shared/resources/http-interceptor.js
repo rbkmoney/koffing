ab1727c385431f41f76ce9f010103dffc7938da8
Andrey Kasalinskiy <kasalinskiy.rbk@gmail.com>
2016-10-26 19:12:12 +0300

    FE-69: added condition

--- app/shared/resources/http-interceptor.js
+++ app/shared/resources/http-interceptor.js
@@ -1,14 +1,14 @@
 resources.factory('httpInterceptor', function($injector) {
     return {
         responseError: function(error) {
-            const modalMsg = $injector.get('modalMsg');
-
-            modalMsg.open([
-                'Error code: ' + error.status,
-                'Url: ' + error.config.url,
-                'Method: ' + error.config.method,
-            ], 'Ошибка http-запроса');
-
+            if (error.config) {
+                const modalMsg = $injector.get('modalMsg');
+                modalMsg.open([
+                    'Error code: ' + error.status,
+                    'Url: ' + error.config.url,
+                    'Method: ' + error.config.method,
+                ], 'Ошибка http-запроса');
+            }
             return error;
         }
     };
