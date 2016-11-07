a38c01c7885de133a90145fa8f44f2314a757ac6
Max Minin <m.minin@rbkmoney.com>
2016-11-07 14:41:39 +0300

    Добавление bootstrap + gentelella в koffing angular2. Добавление первичных компонентов (#30)
    FE-73:
    * настроена сборка webpack
    * добавлены первоначальные компоненты Angular2
    * добавлены bootstrap+gentelella

---
+++ src/app/components/root/root.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: './root.component.pug',
+    styleUrls: ['./root.component.css'],
+    encapsulation: ViewEncapsulation.None
+})
+
+export class RootComponent { }
