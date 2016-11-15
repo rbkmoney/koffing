ec910f3dba846143eab33de49ad1db4e50b07922
Ildar Galeev <KeinAsylum@gmail.com>
2016-11-16 00:07:44 +0300

    FE-84: added custom styles. Simplified analytics component

--- src/app/components/analytics/analytics.component.ts
+++ src/app/components/analytics/analytics.component.ts
@@ -1,8 +1,8 @@
+import * as _ from 'lodash';
 import {Component, OnInit} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 import {Shop, ShopItem} from './../../services/shop/shop';
 import {ShopService} from './../../services/shop/shop.service';
-import * as _ from 'lodash';
 
 @Component({
     selector: 'analytics',
@@ -14,24 +14,27 @@ export class AnalyticsComponent implements OnInit {
     public selectedShopID: string;
     public shopItems: Array<ShopItem> = [];
 
-    constructor(private route: ActivatedRoute,
-                private router: Router,
-                private shopService: ShopService) {
+    constructor(private route: ActivatedRoute, private router: Router, private shopService: ShopService) {
     }
 
     ngOnInit() {
-        const shopID = this.route.snapshot.params['shopID'];
-        if (shopID) {
-            this.selectedShopID = shopID;
-        }
         this.shopService.getShops().then((shops: Shop[]) => {
+            const routeShopID = this.route.snapshot.params['shopID'];
             this.shopItems = _.map(shops, (shop: Shop) => new ShopItem(shop.shopID, shop.shopDetails.name));
+            this.selectedShopID = routeShopID ? routeShopID : this.shopItems[0].value;
+            this.navigate();
         });
     }
 
     onSelectShop(shopItem: ShopItem) {
         this.selectedShopID = shopItem.value;
-        this.router.navigate(['analytics', this.selectedShopID, 'dashboard']);
+        this.navigate();
+    }
+
+    navigate() {
+        const hasChildren = this.route.children.length > 0;
+        const childComponent = hasChildren ? this.route.children[0].routeConfig.path : 'dashboard';
+        this.router.navigate(['analytics', this.selectedShopID, childComponent]);
     }
 
 }
