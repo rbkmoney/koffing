278f6e9ee27a404b21b456f153cb872aa124f5f9
Ildar Galeev <KeinAsylum@gmail.com>
2016-11-16 13:47:15 +0300

    FE-75: added keycloak integration. Added appConfig.json support (#34)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-import { HttpModule } from "@angular/http";
-
+import { Http, XHRBackend, RequestOptions, HttpModule } from '@angular/http';
 import { RootComponent } from './components/root/root.component.ts';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TopPanelComponent } from './components/top-panel/top-panel.component.ts';
-import { AnalyticsComponent } from './components/analytics/analytics.component';
-import { ShopsComponent } from "./components/shops/shops.component";
-import { ClaimsComponent } from "./components/shops/claims/claims.component";
+import { ShopsComponent } from './components/shops/shops.component';
+import { ClaimsComponent } from './components/shops/claims/claims.component';
 import { ShopCreationComponent } from './components/shops/claims/shop-creation/shop-creation.component';
 import { ShopModificationComponent } from './components/shops/claims/shop-modification/shop-modification.component';
 import { ModificationDetailComponent } from './components/shops/claims/modification-detail/modification-detail.component';
-
 import { AnalyticsModule } from './components/analytics/analytics.module';
-import { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule } from './app-routing.module';
+import { KeycloakHttpInterceptor } from '../keycloak/keycloak-http.interceptor';
+import { ConfigService } from './config.service';
 
 @NgModule({
     imports: [
@@ -31,6 +30,22 @@ import { AppRoutingModule } from "./app-routing.module";
         ShopCreationComponent,
         ShopModificationComponent,
         ModificationDetailComponent
+    ],
+    providers: [
+        ConfigService,
+        {
+            provide: Http,
+            useFactory: (
+                backend: XHRBackend, defaultOptions: RequestOptions
+            ) => new KeycloakHttpInterceptor(backend, defaultOptions),
+            deps: [XHRBackend, RequestOptions]
+        },
+        {
+            provide: APP_INITIALIZER,
+            useFactory: (config: ConfigService) => () => config.load(),
+            deps: [ConfigService],
+            multi: true
+        }
     ],
     bootstrap: [ RootComponent ]
 })
---
+++ src/keycloak/keycloak-http.interceptor.ts
@@ -0,0 +1,97 @@
+import {Injectable} from '@angular/core';
+import {Http, ConnectionBackend, RequestOptions, Request, RequestOptionsArgs, Response, Headers} from '@angular/http';
+import {KeycloakService} from './keycloak.service';
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class KeycloakHttpInterceptor extends Http {
+
+    constructor(connectionBackend: ConnectionBackend, defaultOptions: RequestOptions) {
+        super(connectionBackend, defaultOptions);
+    }
+
+    private configureRequest(f: Function, url: string | Request, options: RequestOptionsArgs, body?: any) {
+        const tokenPromise: Promise<string> = this.getToken();
+        const tokenObservable: Observable<string> = Observable.fromPromise(tokenPromise);
+        const tokenUpdateObservable: Observable<any> = Observable.create((observer: any) => {
+            if (!options) {
+                const headers = new Headers();
+                options = new RequestOptions({headers: headers});
+            }
+            this.setHeaders(options);
+            observer.next();
+            observer.complete();
+        });
+        const requestObservable: Observable<Response> = Observable.create((observer: any) => {
+            let result: any;
+            if (body) {
+                result = f.apply(this, [url, body, options]);
+            } else {
+                result = f.apply(this, [url, options]);
+            }
+            result.subscribe((response: any) => {
+                observer.next(response);
+                observer.complete();
+            });
+        });
+        return <Observable<Response>>Observable
+            .merge(tokenObservable, tokenUpdateObservable, requestObservable, 1)
+            .filter((response) => response instanceof Response);
+    }
+
+    private getToken(): Promise<string> {
+        const minValidity = 5;
+        return new Promise<string>((resolve, reject) => {
+            if (KeycloakService.auth.token) {
+                KeycloakService.auth.updateToken(minValidity)
+                    .success(() => resolve(<string>KeycloakService.auth.token))
+                    .error(() => reject('Failed to refresh token'));
+            }
+        });
+    }
+
+    private setHeaders(options: RequestOptionsArgs) {
+        options.headers.set('Authorization', 'Bearer ' + KeycloakService.auth.token);
+        options.headers.set('X-Request-ID', this.guid());
+    }
+
+    private guid(): string {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        }
+
+        return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+    }
+
+    request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
+        return this.configureRequest(super.request, url, options);
+    }
+
+    get(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        return this.configureRequest(super.get, url, options);
+    }
+
+    post(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
+        return this.configureRequest(super.post, url, options, body);
+    }
+
+    put(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
+        return this.configureRequest(super.put, url, options, body);
+    }
+
+    delete(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        return this.configureRequest(super.delete, url, options);
+    }
+
+    patch(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
+        return this.configureRequest(super.patch, url, options, body);
+    }
+
+    head(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        return this.configureRequest(super.head, url, options);
+    }
+
+    options(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        return this.configureRequest(super.options, url, options);
+    }
+}
