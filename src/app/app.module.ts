e40694e7b3418344747cfe29bc768d19ccf45716
Max Minin <m.minin@rbkmoney.com>
2016-11-17 16:14:07 +0300

    FE-72: Перенос функциональности "Мои магазины" в koffing (#35)
    * FE-72:
    * удалён временный компонент ContentComponent
    * добавлены компоненты Shops, Analytics, Claims
    * настроен базовый роутинг
    
    * FE-72:
    * добавлены сервисы для магазинов, категорий
    * добавлены http-запросы для магазинов, категорий
    * доработан компонент shops - получаемые данные отображаются на шаблоне
    
    * FE-72:
    * добавлены компоненты для раздела Мои магазины
    * получение данных для сущности Claim
    
    * FE-72:
    * отображение Claim
    * раздел Мои магазины вынесен в отдельный модуль
    
    * FE-72:
    * добавлены компоненты для добавления и редактирования магазина
    * добавлены необходимые запросы в сервисах
    * учтены замечания по стилю
    
    * FE-72: fix console errors
    
    * FE-72: правки в pull-реквесту
    
    * FE-72: убраны константы, для разрешения путей теперь используется config.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,32 +4,24 @@ import { Http, XHRBackend, RequestOptions, HttpModule } from '@angular/http';
 import { RootComponent } from './components/root/root.component.ts';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TopPanelComponent } from './components/top-panel/top-panel.component.ts';
-import { ShopsComponent } from './components/shops/shops.component';
-import { ClaimsComponent } from './components/shops/claims/claims.component';
-import { ShopCreationComponent } from './components/shops/claims/shop-creation/shop-creation.component';
-import { ShopModificationComponent } from './components/shops/claims/shop-modification/shop-modification.component';
-import { ModificationDetailComponent } from './components/shops/claims/modification-detail/modification-detail.component';
 import { AnalyticsModule } from './components/analytics/analytics.module';
 import { AppRoutingModule } from './app-routing.module';
 import { KeycloakHttpInterceptor } from '../keycloak/keycloak-http.interceptor';
 import { ConfigService } from './config.service';
+import { ShopsModule } from './components/shops/shops.module';
 
 @NgModule({
     imports: [
         BrowserModule,
         AppRoutingModule,
         AnalyticsModule,
+        ShopsModule,
         HttpModule
     ],
     declarations: [
         RootComponent,
         SidebarComponent,
-        TopPanelComponent,
-        ShopsComponent,
-        ClaimsComponent,
-        ShopCreationComponent,
-        ShopModificationComponent,
-        ModificationDetailComponent
+        TopPanelComponent
     ],
     providers: [
         ConfigService,
--- src/app/components/shops/claims/claims.component.ts
+++ src/app/components/shops/claims/claims.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ShopCreationComponent } from './shop-creation/shop-creation.component';
 import { ShopModificationComponent } from './shop-modification/shop-modification.component';
-
 import { Claim } from './../../../services/claim/claim';
 import { ClaimService } from './../../../services/claim/claim.service';
 
 @Component({
     selector: 'claims',
     templateUrl: './claims.component.pug',
-    styleUrls: ['./claims.component.css'],
     providers: [
         ClaimService
     ]
@@ -17,7 +14,7 @@ import { ClaimService } from './../../../services/claim/claim.service';
 
 export class ClaimsComponent implements OnInit {
     public claim: Claim;
-    public changeset: any[] = [];
+    public changeset: any[];
     public showClaimInfo: boolean = false;
 
     constructor(
@@ -27,12 +24,23 @@ export class ClaimsComponent implements OnInit {
     getClaim(): void {
         this.claimService.getClaim().then(
             aClaim => {
-                // debugger;
                 this.claim = aClaim;
                 this.changeset = aClaim.changeset;
                 this.showClaimInfo = true;
             }
         )
+    }
+
+    revoke(): void {
+        let revokeDetails = {
+            reason: 'test' //TODO: реализовать
+        };
+
+        this.claimService.revokeClaim(this.claim.id, revokeDetails).then(
+            (statusText) => {
+                this.showClaimInfo = false;
+            }
+        );
     }
 
     ngOnInit(): void {
