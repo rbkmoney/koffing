f7967818e469389c860e0bed596f55dd0c9e979e
Max Minin <m.minin@rbkmoney.com>
2016-11-30 18:59:14 +0300

    FE-90: Настройка отображения графиков (#43)
    * FE-90: добавлены цвета для всех графиков
    
    * FE-90:
    * изменены цветовые сочетания
    * уменьшен шрифт подписей для графиков line
    * исправлена обработка данных в payment-method.component.ts

---
+++ src/app/components/analytics/dashboard/chart-options.const.ts
@@ -0,0 +1,33 @@
+export class CHART_OPTIONS {
+
+    public static LINE: any = {
+        COLORS: {
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(42,63,83,0.7)',
+            pointBackgroundColor: 'rgba(255,90,90,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(255,90,90,1)'
+        }
+    };
+
+    public static DOUGHNUT: any = {
+        COLORS: {
+            backgroundColor : [
+                'rgba(0,102,204,1)', // blue
+                'rgba(255,90,90,1)', // red
+                'rgba(255,153,51,1)', // orange
+                'rgba(153,153,153,1)', // grey
+                'rgba(51,102,0,1)' // green
+            ],
+            hoverBackgroundColor: [
+                'rgba(0,102,204,0.7)', // blue
+                'rgba(255,90,90,0.7)', // red
+                'rgba(255,153,51,0.7)', // orange
+                'rgba(153,153,153,0.7)', // grey
+                'rgba(51,102,0,0.7)' // green
+            ]
+        }
+    };
+
+}
