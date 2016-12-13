f522c31d890e3df293d9a34dab31b7efecbf7bb0
Andrey Kasalinskiy <kasalinskiy.rbk@gmail.com>
2016-12-13 12:38:07 +0300

    FE-120: http errors handle

--- src/app/broadcaster/services/http-error-broadcaster.service.ts
+++ src/app/broadcaster/services/http-error-broadcaster.service.ts
@@ -13,7 +13,7 @@ export class HttpErrorBroadcaster {
     public fire(status: number) {
         let detail: string = '';
         if (status >= 500 && status < 600) {
-            detail = 'Произошла ошибка на сервере. Повторите действие.';
+            detail = 'Произошла ошибка на сервере. Повторите действие позже.';
         }
         this.broadcaster.broadcast(HttpErrorBroadcaster, {status, detail});
     }
