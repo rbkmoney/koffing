71b502442257efd81d38ff43521a4820426221d8
Max Minin <m.minin@rbkmoney.com>
2016-12-14 18:36:14 +0300

    FE-118: убран отладочный код

--- src/app/common/services/slim-bar.service.ts
+++ src/app/common/services/slim-bar.service.ts
@@ -4,51 +4,30 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class KofSlimBarService {
     private startedProcesses: number;
-    private id: number;
 
     constructor(
         private slimLoadingBarService: SlimLoadingBarService
     ) {
         this.startedProcesses = 0;
-
-        this.printId('created');
     }
 
     public start() {
         if (this.startedProcesses > 0) {
             this.increment();
             this.startedProcesses++;
-
-            this.printId('started again');
         } else {
             this.slimLoadingBarService.start();
             this.startedProcesses = 1;
-
-            this.printId('started');
         }
     }
 
     public stop() {
-        --this.startedProcesses;
-
-        this.printId('stopped');
-
-        if (this.startedProcesses === 0) {
-            this.printId('completed');
-
+        if (--this.startedProcesses === 0) {
             this.slimLoadingBarService.complete();
         }
     }
 
     private increment() {
         this.slimLoadingBarService.progress++;
-    }
-
-    private printId(message: string = 'no message') {
-        if (!this.id) {
-            this.id = Math.round(Math.random() * 100);
-        }
-
-        console.log(`${message} ${this.id}, proccesses: ${this.startedProcesses}`);
     }
 }
