cfb82afe1b301bb4c84a2fe2a531432fc7e945ff
Andrey Kasalinskiy <kasalinskiy.rbk@gmail.com>
2016-12-14 16:06:22 +0300

    FE-120: remove modal in case route change

--- src/app/root/components/http-error-handle/http-error-handle.component.ts
+++ src/app/root/components/http-error-handle/http-error-handle.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 import { Message } from 'primeng/primeng';
 
 import { HttpErrorBroadcaster } from 'kof-modules/broadcaster/broadcaster.module';
