132f382dc131af2ba499c167ce2b52037ccaf51c
Andrey Kasalinskiy <kasalinskiy.rbk@gmail.com>
2017-01-17 13:33:59 +0300

    FE-164: before pull

--- src/app/management/components/management-container/contracts/contracts.component.ts
+++ src/app/management/components/management-container/contracts/contracts.component.ts
@@ -1,11 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import { ContractService } from 'koffing/backend/services/contract.service';
+import { Contract } from 'koffing/backend/classes/contract.class';
 
 @Component({
     templateUrl: 'contracts.component.pug'
 })
 export class ContractsComponent implements OnInit {
 
+    @ViewChild('input')
+    public input: ElementRef;
+
+    public contracts: Contract[] = [];
+    public isLoading: boolean;
+
+    constructor(private contractService: ContractService) {}
+
     public ngOnInit() {
-        console.log('ContractsComponent');
+        this.isLoading = true;
+        this.contractService.getContracts().then(response => {
+            this.contracts = response;
+            this.isLoading = false;
+        });
+    }
+
+    public expandInfo(e: any) {
+        // console.log(e);
+        console.log(this.input);
     }
 }
