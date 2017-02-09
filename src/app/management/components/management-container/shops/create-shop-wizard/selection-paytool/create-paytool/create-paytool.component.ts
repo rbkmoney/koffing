9556f1d111ba67f6225f4ffb94c3caa99f795d72
Max Minin <m.minin@rbkmoney.com>
2017-02-09 18:34:28 +0300

    FE-179: подсказки для банковских полей на форме средства вывода

--- src/app/management/components/management-container/shops/create-shop-wizard/selection-paytool/create-paytool/create-paytool.component.ts
+++ src/app/management/components/management-container/shops/create-shop-wizard/selection-paytool/create-paytool/create-paytool.component.ts
@@ -1,7 +1,9 @@
-import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { PayoutToolBankAccount } from 'koffing/backend/classes/payout-tool-bank-account.class';
 import { BankAccount } from 'koffing/backend/classes/bank-account.class';
+import { SuggestionSettings } from 'koffing/management/classes/suggestion-settings.const';
 
 @Component({
     selector: 'kof-create-paytool',
@@ -14,12 +16,16 @@ export class CreatePayoutToolComponent implements OnInit {
 
     public payoutTool: PayoutToolBankAccount;
 
+    @ViewChild('form')
+    private form: NgForm;    
+    
     public ngOnInit() {
         this.payoutTool = this.getInstance();
+        this.initBankSuggestions();
     }
 
-    public checkForm(form: any) {
-        if (form.valid) {
+    public checkForm() {
+        if (this.form.valid) {
             this.onPayoutToolReady.emit(this.payoutTool);
         }
     }
@@ -34,4 +40,35 @@ export class CreatePayoutToolComponent implements OnInit {
         instance.bankAccount = bankAccount;
         return instance;
     }
+
+    private handleBankSuggestion(suggestion: BankSuggestion) {
+        const bankNameControl = this.form.controls['bankName'];
+        const bankPostAccountControl = this.form.controls['bankPostAccount'];
+        const bankBikControl = this.form.controls['bankBik'];
+
+        if (suggestion) {
+            bankNameControl.setValue(suggestion.unrestricted_value);
+            if (suggestion.data) {
+                bankPostAccountControl.setValue(suggestion.data.correspondent_account);
+                bankBikControl.setValue(suggestion.data.bic);
+            }
+        } else {
+            bankNameControl.setValue('');
+            bankPostAccountControl.setValue('');
+            bankBikControl.setValue('');
+        }
+
+        this.checkForm();
+    }
+
+    private initBankSuggestions() {
+        (<JQuerySuggestions> $('input.bank-suggestions')).suggestions(<SuggestionsParams> {
+            serviceUrl: SuggestionSettings.serviceUrl,
+            token: SuggestionSettings.token,
+            type: SuggestionSettings.bankType,
+            count: 5,
+            onSelect: this.handleBankSuggestion.bind(this)
+        });
+    }    
+    
 }
