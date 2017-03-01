232e3f36a00826b6e8da6ab1d684a8e7fa8783b3
Andrey Kasalinskiy <kasalinskiy.rbk@gmail.com>
2017-03-01 15:44:09 +0300

    FE-181: revert customer service

--- src/app/backend/services/customer.service.ts
+++ src/app/backend/services/customer.service.ts
@@ -9,36 +9,39 @@ import { ConfigService } from './config.service';
 @Injectable()
 export class CustomerService {
 
-    constructor(
-        private http: Http,
-        private config: ConfigService
-    ) {}
+    constructor(private http: Http, private config: ConfigService) { }
 
     public getPaymentMethod(shopID: number, requestParams: RequestParams): Promise<any> {
-        const fromTime = moment(requestParams.fromTime).utc().format();
-        const toTime = moment(requestParams.toTime).utc().format();
+        let params = new URLSearchParams();
 
-        const params = new URLSearchParams();
+        let fromTime = moment(requestParams.fromTime).utc().format();
+        let toTime = moment(requestParams.toTime).utc().format();
+
         params.set('fromTime', fromTime);
         params.set('toTime', toTime);
         params.set('splitUnit', requestParams.splitUnit);
         params.set('splitSize', requestParams.splitSize);
         params.set('paymentMethod', requestParams.paymentMethod);
 
-        return this.http.get(`${this.config.capiUrl}/analytics/shops/${shopID}/customers/stats/payment_method`, {search: params})
+        return this.http.get(`${this.config.capiUrl}/analytics/shops/${shopID}/customers/stats/payment_method`, {
+            search: params
+        })
             .toPromise()
             .then((response) => response.json());
     }
 
     public getRate(shopID: number, requestParams: RequestParams): Promise<any> {
-        const fromTime = moment(requestParams.fromTime).utc().format();
-        const toTime = moment(requestParams.toTime).utc().format();
+        let params = new URLSearchParams();
 
-        const params = new URLSearchParams();
+        let fromTime = moment(requestParams.fromTime).utc().format();
+        let toTime = moment(requestParams.toTime).utc().format();
+
         params.set('fromTime', fromTime);
         params.set('toTime', toTime);
 
-        return this.http.get(`${this.config.capiUrl}/analytics/shops/${shopID}/customers/stats/rate`, {search: params})
+        return this.http.get(`${this.config.capiUrl}/analytics/shops/${shopID}/customers/stats/rate`, {
+            search: params
+        })
             .toPromise()
             .then((response) => response.json());
     }
