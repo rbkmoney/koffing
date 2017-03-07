6bc7db2c0734810fcefac16aa08bf9734f9acc54
Max Minin <m.minin@rbkmoney.com>
2017-03-07 19:31:49 +0300

    FE-199: изменение заявки - в процессе

--- src/app/backend/classes/shop-details.class.ts
+++ src/app/backend/classes/shop-details.class.ts
@@ -1,7 +1,31 @@
+import * as _ from 'lodash';
+
 import { ShopLocation } from './shop-location.class';
 
 export class ShopDetails {
     public name: string;
     public description: string;
     public location: ShopLocation;
+
+    constructor() {
+        this.location = new ShopLocation();
+    }
+
+    public getUpdated(other: ShopDetails): ShopDetails {
+        const that = new ShopDetails();
+        that.name = !_.isEqual(this.name, other.name) ? other.name : this.name;
+        that.description = !_.isEqual(this.description, other.description) ? other.description : this.description;
+        if (other.location) {
+            if (this.location.locationType !== other.location.locationType) {
+                that.location = _.clone(other.location);
+            } else {
+                _.forEach(_.keys(this.location), (key) => {
+                    that.location[key] = !_.isEqual(this.location[key], other.location[key]) ? other.location[key] : this.location[key];
+                });
+            }
+        } else {
+            that.location = _.clone(this.location);
+        }
+        return that;
+    }
 }
