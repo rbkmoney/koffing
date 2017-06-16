f48dd287d03fb4968f765de4da4dbcd4a655dc69
Ildar Galeev <KeinAsylum@gmail.com>
2017-06-16 12:57:54 +0300

    FE-361: Simplify mailto template. Changed account datatype.

--- src/app/root/components/top-panel/top-panel.component.ts
+++ src/app/root/components/top-panel/top-panel.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { AuthService } from 'koffing/auth/auth.module';
 import { ToggleMenuBroadcaster } from 'koffing/broadcaster/broadcaster.module';
+import { ConfigService } from 'koffing/backend/services/config.service';
 
 @Component({
     selector: 'kof-top-panel',
@@ -11,10 +12,11 @@ import { ToggleMenuBroadcaster } from 'koffing/broadcaster/broadcaster.module';
 export class TopPanelComponent {
 
     public profileName: string = AuthService.getAccountInfo().profileName;
-    public mailHref: string = this.generateMailHref();
+    public mailHref: string = `mailto:${this.configService.supportEmail}`;
 
     constructor(
-        private toggleMenuBroadcaster: ToggleMenuBroadcaster
+        private toggleMenuBroadcaster: ToggleMenuBroadcaster,
+        private configService: ConfigService
     ) { }
 
     public logout() {
@@ -23,12 +25,5 @@ export class TopPanelComponent {
 
     public toggleMenu() {
         this.toggleMenuBroadcaster.fire();
-    }
-
-    private generateMailHref() {
-        const email = 'test@example.com';
-        const subject = 'Проблема с использованием личного кабинета';
-        const body = `Опишите свою проблему\n\n\n--------------------\nEmail, на который зарегистрирован личный кабинет: ${AuthService.getAccountInfo().email}`;
-        return `mailto:${email}?subject=${encodeURI(subject)}&body=${encodeURI(body)}`;
     }
 }
