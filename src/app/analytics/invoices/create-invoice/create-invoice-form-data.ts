ce57c76d1034d7a9d810aea6a74e3ec5f8e43b91
Ildar Galeev <KeinAsylum@gmail.com>
2017-06-26 16:33:06 +0300

    FE-379: Bump to angular 4. Added min/max validators.

--- src/app/analytics/invoices/create-invoice/create-invoice-form-data.ts
+++ src/app/analytics/invoices/create-invoice/create-invoice-form-data.ts
@@ -2,5 +2,5 @@ export class CreateInvoiceFormData {
     public amount: number;
     public dueDate: Date;
     public product: string;
-    public description: string;
+    public description?: string;
 }
