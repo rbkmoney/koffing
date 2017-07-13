ea5857c8a28eaff158ea39c84c89a6e60a090713
Andrey Kasalinskiy <kasalinskiy.rbk@gmail.com>
2017-07-13 16:13:36 +0300

    FE-393: analytics.module order

--- src/app/analytics/analytics.module.ts
+++ src/app/analytics/analytics.module.ts
@@ -7,15 +7,10 @@ import { CalendarModule } from 'primeng/components/calendar/calendar';
 import { BackendModule } from 'koffing/backend/backend.module';
 import { CommonModule } from 'koffing/common/common.module';
 import { AnalyticsRoutingModule } from './analytics-routing.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { InvoicesComponent } from './invoices/invoices.component';
-import { InvoiceTemplatesComponent } from './invoice-templates/invoice-templates.component';
 import { RegistryComponent } from './registry/registry.component';
 import { PaginateComponent } from './invoices/paginate/paginate.component';
-import { SearchFormComponent } from './invoices/search-form/search-form.component';
-import { SearchFormInvoiceTemplatesComponent } from './invoice-templates/search-form-invoice-templates/search-form-invoice-templates.component';
-import { SearchResultComponent } from './invoices/search-result/search-result.component';
-import { SearchResultInvoiceTemplatesComponent } from './invoice-templates/search-result-invoice-templates/search-result-invoice-templates.component';
 import { PaginationPipe } from './invoices/paginate/pagination.pipe';
 import { RoubleCurrencyPipe } from './rouble-currency.pipe';
 import { AnalyticsComponent } from './analytics.component';
@@ -24,17 +19,23 @@ import { LineChartComponent } from './dashboard/line-chart/line-chart.component'
 import { DoughnutChartComponent } from './dashboard/doughnut-chart/doughnut-chart.component';
 import { StatisticPanelComponent } from './statistic-panel/statistic-panel.component';
 import { InvoiceStatusPipe } from './invoices/invoice-statuses.pipe';
-import { SearchDetailsComponent } from './invoices/search-result/search-details/search-details.component';
-import { SearchDetailsInvoiceTemplateComponent } from './invoice-templates/search-result-invoice-templates/search-details-invoice-template.component';
-import { PaymentInfoComponent } from './invoices/search-result/payment-info/payment-info.component';
 import { PaymentStatusPipe } from './invoices/payment-statuses.pipe';
+import { CreatePaymentLinkComponent } from './invoices/search-result/search-details/create-payment-link.component';
+import { InvoicesComponent } from './invoices/invoices.component';
+import { ActionsComponent } from './invoices/actions/actions.component';
 import { CreateInvoiceComponent } from './invoices/create-invoice/create-invoice.component';
+import { SearchFormComponent } from './invoices/search-form/search-form.component';
+import { SearchResultComponent } from './invoices/search-result/search-result.component';
+import { SearchDetailsComponent } from './invoices/search-result/search-details/search-details.component';
+import { PaymentInfoComponent } from './invoices/search-result/payment-info/payment-info.component';
+import { InvoiceTemplatesComponent } from './invoice-templates/invoice-templates.component';
+import { ActionsInvoiceTemplatesComponent } from './invoice-templates/actions-invoice-templates/actions-invoice-templates.component';
+import { SearchFormInvoiceTemplatesComponent } from './invoice-templates/search-form-invoice-templates/search-form-invoice-templates.component';
+import { SearchResultInvoiceTemplatesComponent } from './invoice-templates/search-result-invoice-templates/search-result-invoice-templates.component';
+import { SearchDetailsInvoiceTemplateComponent } from './invoice-templates/search-result-invoice-templates/search-details-invoice-template.component';
 import { CreateInvoiceTemplateComponent } from './invoice-templates/create-invoice-template/create-invoice-template.component';
 import { UpdateInvoiceTemplateComponent } from './invoice-templates/update-invoice-template/update-invoice-template.component';
 import { DeleteInvoiceTemplateComponent } from './invoice-templates/delete-invoice-template/delete-invoice-template.component';
-import { ActionsComponent } from './invoices/actions/actions.component';
-import { ActionsInvoiceTemplatesComponent } from './invoice-templates/actions-invoice-templates/actions-invoice-templates.component';
-import { CreatePaymentLinkComponent } from './invoices/search-result/search-details/create-payment-link.component';
 
 @NgModule({
     imports: [
@@ -46,18 +47,12 @@ import { CreatePaymentLinkComponent } from './invoices/search-result/search-deta
         BackendModule
     ],
     declarations: [
-        DateRangeSelectorComponent,
+        BaseChartDirective,
         AnalyticsComponent,
         DashboardComponent,
-        InvoicesComponent,
-        InvoiceTemplatesComponent,
         RegistryComponent,
+        DateRangeSelectorComponent,
         PaginateComponent,
-        SearchFormComponent,
-        SearchFormInvoiceTemplatesComponent,
-        SearchResultComponent,
-        SearchResultInvoiceTemplatesComponent,
-        BaseChartDirective,
         InvoiceStatusPipe,
         PaymentStatusPipe,
         RoubleCurrencyPipe,
@@ -65,16 +60,22 @@ import { CreatePaymentLinkComponent } from './invoices/search-result/search-deta
         LineChartComponent,
         DoughnutChartComponent,
         StatisticPanelComponent,
-        SearchDetailsComponent,
-        SearchDetailsInvoiceTemplateComponent,
-        PaymentInfoComponent,
+        InvoicesComponent,
+        ActionsComponent,
         CreateInvoiceComponent,
+        CreatePaymentLinkComponent,
+        SearchFormComponent,
+        SearchResultComponent,
+        SearchDetailsComponent,
+        PaymentInfoComponent,
+        InvoiceTemplatesComponent,
+        ActionsInvoiceTemplatesComponent,
         CreateInvoiceTemplateComponent,
         UpdateInvoiceTemplateComponent,
         DeleteInvoiceTemplateComponent,
-        ActionsComponent,
-        ActionsInvoiceTemplatesComponent,
-        CreatePaymentLinkComponent
+        SearchFormInvoiceTemplatesComponent,
+        SearchResultInvoiceTemplatesComponent,
+        SearchDetailsInvoiceTemplateComponent,
     ]
 })
 export class AnalyticsModule { }
