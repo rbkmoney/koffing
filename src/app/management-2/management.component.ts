7d60a1625c15cd9a27e16684012714c5a33a44e3
Ildar Galeev <KeinAsylum@gmail.com>
2017-07-14 18:52:08 +0300

    FE-401: Line fix.

--- src/app/management-2/management.component.ts
+++ src/app/management-2/management.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 import { ClaimService } from 'koffing/backend/claim.service';
 import { Claim } from 'koffing/backend';
 
