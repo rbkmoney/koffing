70046d7dcd8de652c988d3356697a6ceeb31e9ce
Andrey Kasalinskiy <kasalinskiy.rbk@gmail.com>
2017-08-01 16:10:17 +0300

    FE-414: move logic from component to service

--- src/app/analytics/invoice-form/invoice-form.service.ts
+++ src/app/analytics/invoice-form/invoice-form.service.ts
@@ -1,28 +1,59 @@
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Injectable } from '@angular/core';
 import { reduce } from 'lodash';
 
 import { Product } from './product';
+import { INVOICE_TYPES } from './invoice-types';
 
 @Injectable()
 export class InvoiceFormService {
 
     public form: FormGroup;
 
+    get cart(): FormArray {
+        return this.form.get('cart') as FormArray;
+    }
+
     constructor(private fb: FormBuilder) {
         this.form = this.initForm();
+        this.form.controls.selectedInvoiceType.valueChanges.subscribe((change) => this.initCart(change));
         this.form.controls.cart.valueChanges.subscribe((change) => this.calculateCartAmount(change));
+    }
+
+    public addProduct() {
+        this.cart.push(this.getProduct());
+    }
+
+    public removeProduct(index: number) {
+        if (this.cart.length > 1) {
+            this.cart.removeAt(index);
+        }
     }
 
     private initForm(): FormGroup {
         return this.fb.group({
-            amount: ['', [ Validators.required ]],
+            amount: ['', [ Validators.required, Validators.min(10) ]],
             product: ['', [ Validators.required, Validators.maxLength(100) ]],
             description: ['', [ Validators.maxLength(1000) ]],
             dueDate: ['', [ Validators.required ]],
             selectedInvoiceType: ['', [ Validators.required ]],
             cart: this.fb.array([]),
             cartAmount: ['']
+        });
+    }
+
+    private initCart(changeType: string) {
+        this.form.setControl('cart', this.fb.array([]));
+        if (changeType === INVOICE_TYPES.cart) {
+            this.addProduct();
+        }
+    }
+
+    private getProduct(): FormGroup {
+        return this.fb.group({
+            quantity: ['', [ Validators.required, Validators.min(1) ]],
+            price: ['', [ Validators.required, Validators.min(10) ]],
+            description: ['', [ Validators.required, Validators.maxLength(1000) ]],
         });
     }
 
