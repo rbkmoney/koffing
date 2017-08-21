7e227d106f1069cdf49d766563aac6249c1d95a4
Andrey Kasalinskiy <kasalinskiy.rbk@gmail.com>
2017-08-21 17:23:33 +0300

    FE-424: remake sidebar

--- src/app/root/components/sidebar/sidebar.component.ts
+++ src/app/root/components/sidebar/sidebar.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'kof-sidebar',
-    templateUrl: './sidebar.component.pug'
+    templateUrl: './sidebar.component.pug',
+    styles: [`
+        .left_col { width: 100%; }
+        .menu_section > ul { margin-top: 30px; }
+    `]
 })
 export class SidebarComponent { }
