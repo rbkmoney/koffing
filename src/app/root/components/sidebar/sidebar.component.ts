b599e745c8f04da296b34d4bcf7e1f3506249f41
Andrey Kasalinskiy <kasalinskiy.rbk@gmail.com>
2017-08-21 17:29:34 +0300

    FE-424: separate sidebar styles

--- src/app/root/components/sidebar/sidebar.component.ts
+++ src/app/root/components/sidebar/sidebar.component.ts
@@ -3,9 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'kof-sidebar',
     templateUrl: './sidebar.component.pug',
-    styles: [`
-        .left_col { width: 100%; }
-        .menu_section > ul { margin-top: 30px; }
-    `]
+    styleUrls: ['sidebar.component.less']
 })
 export class SidebarComponent { }
