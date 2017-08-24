bbfc8fee5376bca9302a8e0e1f3d35f27bce5fdd
Andrey Kasalinskiy <kasalinskiy.rbk@gmail.com>
2017-08-24 17:10:51 +0300

    FE-429: add view for cart

--- src/app/invoices/create-invoice/create-invoice.service.ts
+++ src/app/invoices/create-invoice/create-invoice.service.ts
@@ -1,9 +1,10 @@
-import { round, map } from 'lodash';
+import { map } from 'lodash';
 import * as moment from 'moment';
 
 import { InvoiceParams } from 'koffing/backend/requests/invoice-params';
 import { InvoiceLine } from 'koffing/backend/model/invoice-cart/invoice-line';
 import { InvoiceLineTaxVAT } from 'koffing/backend/model/invoice-cart/invoice-line-tax-vat';
+import { CurrencyService } from 'koffing/common/currency.service';
 import { Product } from '../invoice-form/product';
 
 export class CreateInvoiceService {
@@ -17,16 +18,12 @@ export class CreateInvoiceService {
         params.description = formValue.description;
         params.metadata = {};
         params.cart = map(formValue.cart, (product: Product) => {
-            const invoiceLine = new InvoiceLine(product.product, product.quantity, this.toMinor(product.price));
+            const invoiceLine = new InvoiceLine(product.product, product.quantity, CurrencyService.toMinor(product.price));
             if (product.tax) {
                 invoiceLine.taxMode = new InvoiceLineTaxVAT(product.tax);
             }
             return invoiceLine;
         });
         return params;
-    }
-
-    private static toMinor(value: number): number {
-        return round(value * 100);
     }
 }
