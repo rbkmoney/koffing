af58d4797e9f28fe4cd9b7410358b9796167f261
Alexandra Usacheva <lazercore@me.com>
2017-10-12 15:55:11 +0300

    FE-468: PR fix

--- src/app/checkout/checkout-config-form/checkout-config-form.component.ts
+++ src/app/checkout/checkout-config-form/checkout-config-form.component.ts
@@ -45,4 +45,12 @@ export class CheckoutConfigFormComponent implements OnInit, OnChanges {
     public isSelected(holdExpiration: string): boolean {
         return this.form.value.holdExpiration === holdExpiration;
     }
+
+    public checkTerminals() {
+        this.form.setValue({
+            ...this.form.value,
+            paymentFlowHold: false,
+            terminals: !this.form.value.terminals
+        });
+    }
 }
