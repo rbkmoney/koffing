6880e807ba396530f0049b390a7c881fadb4e905
Alexandra Usacheva <lazercore@me.com>
2018-02-14 12:57:34 +0300

    FE-540: remove wallets method from search

--- src/app/invoices/payment-method-label.ts
+++ src/app/invoices/payment-method-label.ts
@@ -3,5 +3,5 @@ import { PAYMENT_METHOD } from 'koffing/backend';
 export const PAYMENT_METHOD_LABEL = {
     [PAYMENT_METHOD.bankCard]: 'Банковская карта',
     [PAYMENT_METHOD.paymentTerminal]: 'Платежный терминал',
-    [PAYMENT_METHOD.digitalWallet]: 'Электронный кошелек'
+    // [PAYMENT_METHOD.digitalWallet]: 'Электронный кошелек'
 };
