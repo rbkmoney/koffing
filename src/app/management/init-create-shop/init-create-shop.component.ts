9843e3ec8f887c31cd7138f0c0bfcdea37982057
Alexandra Usacheva <lazercore@me.com>
2018-02-19 15:51:31 +0300

    FE-553: added entity type chooser

--- src/app/management/init-create-shop/init-create-shop.component.ts
+++ src/app/management/init-create-shop/init-create-shop.component.ts
@@ -1,41 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { BreadcrumbBroadcaster } from 'koffing/broadcaster';
 import { LegalEntityTypeEnum } from 'koffing/backend/model/contract/contractor/legal-entity-type-enum';
-import { InitCreateShopService } from 'koffing/management/init-create-shop/init-create-shop.service';
-import { FormGroup } from '@angular/forms';
 import { SelectItem } from 'koffing/common/select/select-item';
 import { Router } from '@angular/router';
 
 @Component({
     templateUrl: 'init-create-shop.component.pug',
-    providers: [InitCreateShopService]
+    styleUrls: ['init-create-shop.component.less']
 })
 export class InitCreateShopComponent implements OnInit {
 
     public types: SelectItem[] = [
-        {value: LegalEntityTypeEnum.RussianLegalEntity, label: 'Резидент РФ'},
-        {value: LegalEntityTypeEnum.InternationalLegalEntity, label: 'Неризедент РФ'}
+        {value: LegalEntityTypeEnum.RussianLegalEntity, label: 'Юридическое лицо, ведущее деятельность под юрисдикцией РФ'},
+        {value: LegalEntityTypeEnum.InternationalLegalEntity, label: 'Международное юридическое лицо'}
     ];
-    public form: FormGroup;
 
     constructor(
         private breadcrumbBroadcaster: BreadcrumbBroadcaster,
-        private initCreateShopService: InitCreateShopService,
         private router: Router
     ) { }
 
     public ngOnInit() {
-        this.form = this.initCreateShopService.createForm;
         this.breadcrumbBroadcaster.fire([{label: 'Создание магазина'}]);
     }
 
-    public next() {
-        switch (this.form.value.entityType) {
+    public next(entityType: LegalEntityTypeEnum) {
+        switch (entityType) {
             case LegalEntityTypeEnum.RussianLegalEntity:
-                this.router.navigate(['/shop/create/resident'])
+                this.router.navigate(['/shop/create/resident']);
                 break;
             case LegalEntityTypeEnum.InternationalLegalEntity:
-                this.router.navigate(['/shop/create/nonresident'])
+                this.router.navigate(['/shop/create/nonresident']);
                 break;
         }
     }
