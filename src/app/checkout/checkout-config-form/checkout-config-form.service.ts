7e84e20bce1ee2597d3720aeddb6c236d7c9b5ef
Alexandra Usacheva <lazercore@me.com>
2018-02-21 14:37:57 +0300

    Update checkout-config-form.service.ts

--- src/app/checkout/checkout-config-form/checkout-config-form.service.ts
+++ src/app/checkout/checkout-config-form/checkout-config-form.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { HOLD_EXPIRATION } from 'koffing/backend/constants/hold-expiration';
+import { PaymentMethod } from 'koffing/backend';
 
 @Injectable()
 export class CheckoutConfigFormService {
@@ -9,19 +10,28 @@ export class CheckoutConfigFormService {
     public form: FormGroup;
 
     constructor(private fb: FormBuilder) {
-        this.form = this.initForm();
     }
 
-    private initForm(): FormGroup {
+    public initForm(methods: any): FormGroup {
+        let availableMethods = {};
+        if (methods) {
+            availableMethods = methods.reduce((acc: {}, current: PaymentMethod) => {
+                switch (current.method) {
+                    case 'DigitalWallet':
+                        return {...acc, wallets: false};
+                    case 'PaymentTerminal':
+                        return {...acc, terminals: false};
+                }
+            }, {});
+        }
         return this.fb.group({
             name: ['', [Validators.maxLength(30)]],
             description: [''],
             email: [''],
             redirectUrl: [''],
-            paymentFlowHold: [false, [Validators.required] ],
+            paymentFlowHold: [false, [Validators.required]],
             holdExpiration: [HOLD_EXPIRATION.cancel, [Validators.required]],
-            terminals: null,
-            wallets: null
+            ...availableMethods
         });
     }
 }
