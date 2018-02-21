40f48051153e5a14d68b59c601b2d6f0e1f4091b
Alexandra Usacheva <lazercore@me.com>
2018-02-21 16:11:41 +0300

    Update checkout-config-form.service.ts

--- src/app/checkout/checkout-config-form/checkout-config-form.service.ts
+++ src/app/checkout/checkout-config-form/checkout-config-form.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { HOLD_EXPIRATION } from 'koffing/backend/constants/hold-expiration';
-import { PaymentMethod } from 'koffing/backend';
 
 @Injectable()
 export class CheckoutConfigFormService {
@@ -10,9 +9,10 @@ export class CheckoutConfigFormService {
     public form: FormGroup;
 
     constructor(private fb: FormBuilder) {
+        this.form = this.initForm();
     }
 
-    public initForm(methods: any): FormGroup {
+    private initForm(): FormGroup {
         return this.fb.group({
             name: ['', [Validators.maxLength(30)]],
             description: [''],
@@ -20,20 +20,8 @@ export class CheckoutConfigFormService {
             redirectUrl: [''],
             paymentFlowHold: [false, [Validators.required]],
             holdExpiration: [HOLD_EXPIRATION.cancel, [Validators.required]],
-            ...this.getAvailableMethods(methods)
+            wallets: null,
+            terminals: null
         });
-    }
-
-    private getAvailableMethods(methods: PaymentMethod[]) {
-        return methods.reduce((acc: {}, current: PaymentMethod) => {
-            switch (current.method) {
-                case 'DigitalWallet':
-                    return {...acc, wallets: false};
-                case 'PaymentTerminal':
-                    return {...acc, terminals: false};
-                default:
-                    return acc;
-            }
-        }, {});
     }
 }
