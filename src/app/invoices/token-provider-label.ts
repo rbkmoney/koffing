df27283a2cb16f421fedde8a846d72dceeb232d1
Alexandra Usacheva <lazercore@me.com>
2018-05-30 16:30:19 +0300

    Update token-provider-label.ts

--- src/app/invoices/token-provider-label.ts
+++ src/app/invoices/token-provider-label.ts
@@ -1,7 +1,7 @@
 import { TOKEN_PROVIDER } from 'koffing/backend';
 
 export const TOKEN_PROVIDER_LABEL = {
-    [TOKEN_PROVIDER.applepay]: 'Apple pay',
-    [TOKEN_PROVIDER.googlepay]: 'Google pay',
-    [TOKEN_PROVIDER.samsungpay]: 'Samsung pay'
+    [TOKEN_PROVIDER.applepay]: 'Apple Pay',
+    [TOKEN_PROVIDER.googlepay]: 'Google Pay',
+    [TOKEN_PROVIDER.samsungpay]: 'Samsung Pay'
 };
