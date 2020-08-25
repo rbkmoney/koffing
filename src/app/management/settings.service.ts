781b24df4483d0bffd54fb6fcd46b81802020fe8
Aleksandra Usacheva <lazercore@me.com>
2020-08-25 14:18:04 +0300

    dashboard warning

--- src/app/management/settings.service.ts
+++ src/app/management/settings.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SettingsService {
-    setLocalStorageItem(key: string, value: string) {
+    public setLocalStorageItem(key: string, value: string) {
         localStorage.setItem(this.getKeyName(key), value);
     }
 
-    setLocalStorageAllItems(keyValue: { [name: string]: string }) {
+    public setLocalStorageAllItems(keyValue: { [name: string]: string }) {
         for (const [k, v] of Object.entries(keyValue)) {
             this.setLocalStorageItem(k, v);
         }
     }
 
-    getLocalStorageItem(key: string): string {
+    public getLocalStorageItem(key: string): string {
         return localStorage.getItem(this.getKeyName(key));
     }
 
